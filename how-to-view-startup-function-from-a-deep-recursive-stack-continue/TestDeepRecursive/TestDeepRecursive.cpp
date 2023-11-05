dc2491b0991be82cec669ee62cd750799baebe21
bcn <273282378@qq.com>
2023-11-05 20:52:34 +0800

    add how-to-view-startup-function-from-a-deep-recursive-stack-continue

---
+++ how-to-view-startup-function-from-a-deep-recursive-stack-continue/TestDeepRecursive/TestDeepRecursive.cpp
@@ -0,0 +1,17 @@
+﻿void Recursive(int depth)
+{
+    if (depth > 0)
+    {
+        Recursive(--depth);
+    }
+}
+
+void CallRecursive()
+{
+    Recursive(0x7fffffff);
+}
+
+int main()
+{
+    CallRecursive();
+}
