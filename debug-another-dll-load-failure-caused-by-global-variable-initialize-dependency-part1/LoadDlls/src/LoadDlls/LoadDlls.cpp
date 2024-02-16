bc1bc766d48039e7a7df082b5ab4009a4b135dbf
bcn <273282378@qq.com>
2024-02-16 09:51:58 +0800

    debug-another-dll-load-failure-caused-by-global-variable-initialize-dependency-part1

---
+++ debug-another-dll-load-failure-caused-by-global-variable-initialize-dependency-part1/LoadDlls/src/LoadDlls/LoadDlls.cpp
@@ -0,0 +1,58 @@
+﻿#include "windows.h"
+#include <iostream>
+#include <map>
+#include <vector>
+
+typedef void (*PFN_Init)();
+
+std::map<std::string, HMODULE> LoadPlugins(const char* plugins[])
+{
+    std::map<std::string, HMODULE> result;
+    for (int idx = 0; ; ++idx)
+    {
+        const char* plugin = plugins[idx];
+        if (plugin == nullptr)
+        {
+            break;
+        }
+
+        HMODULE module = LoadLibraryA(plugin);
+        if (module == nullptr)
+        {
+            DWORD lastError = GetLastError();
+            std::cout << "[+] load ［" << plugin << "] failed with error " << lastError << std::endl;
+        }
+        else
+        {
+            result[plugin] = module;
+        }
+    }
+
+    return result;
+}
+
+void InitPlugins(const std::map<std::string, HMODULE>& loaded_plugins)
+{
+    for (auto& it : loaded_plugins)
+    {
+        auto init_entry = (PFN_Init)GetProcAddress(it.second, "Init");
+        if (init_entry != nullptr)
+        {
+            init_entry();
+        }
+    }
+}
+
+int main()
+{
+    std::cout << "[+] load plugin start." << std::endl;
+
+    const char* plugins[] = { "dll1.dll", /*"dll2.dll", "dll3.dll",*/ nullptr };
+    auto loaded_module_map = LoadPlugins(plugins);
+
+    std::cout << "[+] load plugin done, press any key to init plugins." << std::endl;
+    system("pause");
+
+    InitPlugins(loaded_module_map);
+    return 0;
+}
---
+++ debug-another-dll-load-failure-caused-by-global-variable-initialize-dependency-part1/LoadDlls/src/dll1/dllmain1.cpp
@@ -0,0 +1,15 @@
+﻿#include <windows.h>
+#include <iostream>
+#include "../common/autorunner.h"
+
+#include "../dll2/exports.h"
+
+void PrintMajorVersion()
+{
+    std::cout << "Major Version of dll2.dll is " << MajorVersion() << std::endl;
+}
+
+BEGIN_AUTO_RUN
+std::cout << "I'm running in dll1.dll, which implicitly depends on dll2.dll." << std::endl;
+END_AUTO_RUN
+
