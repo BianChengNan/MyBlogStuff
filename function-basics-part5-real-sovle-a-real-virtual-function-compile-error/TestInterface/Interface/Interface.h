3342ec80f5859f9718ebb27d09417518f4ff0b3b
bcn <273282378@qq.com>
2024-02-19 22:33:43 +0800

    function-basics-part5-real-sovle-a-real-virtual-function-compile-error

---
+++ function-basics-part5-real-sovle-a-real-virtual-function-compile-error/TestInterface/Interface/Interface.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#ifdef DLL_EXPORT_INTERFACE
+#define DLL_EXPORT_INTERFACE __declspec(dllexport)
+#else
+#define DLL_EXPORT_INTERFACE __declspec(dllimport)
+#endif
+
+class InterfaceBase
+{
+public:
+    virtual ~InterfaceBase() {}
+};
+
+class Interface1 : public InterfaceBase
+{
+public:
+    DLL_EXPORT_INTERFACE virtual void Test1(int);
+    DLL_EXPORT_INTERFACE virtual void Test2(int);
+    DLL_EXPORT_INTERFACE virtual void Test3(int);
+    DLL_EXPORT_INTERFACE virtual void Test4(int);
+};
+
+DLL_EXPORT_INTERFACE InterfaceBase* GetInterface();
---
+++ function-basics-part5-real-sovle-a-real-virtual-function-compile-error/TestInterface/InterfaceExe/InterfaceExe.cpp
@@ -0,0 +1,9 @@
+﻿#include "../Interface/Interface.h"
+int main()
+{
+    Interface1* if1 = dynamic_cast<Interface1*>(GetInterface());
+    if1->Test4(0);
+
+    auto if2 = Interface1();
+    return 0;
+}
