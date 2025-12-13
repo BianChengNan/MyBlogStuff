a3f8c6eaf039af615b0a6976b7929571c04d9247
bcn <273282378@qq.com>
2025-12-13 16:02:47 +0800

    debug-unexpected-crash-and-never-ignore-compiling-warnings

---
+++ debug-unexpected-crash-and-never-ignore-compiling-warnings/TestSwitchCaseCrash/TestSwitchCaseCrash.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <string>
+
+class CResult
+{
+public:
+    CResult(const std::wstring& data_)
+        : data(data_)
+    {
+    }
+
+protected:
+    std::wstring data;
+};
+
+int DoModify()
+{
+    // a lot logic code, return error code
+    return 1;
+}
+
+std::wstring GetErrorStr(int error)
+{
+    switch (error)
+    switch (error)
+    {
+    case 0:
+        return L"Ok";
+    case -1:
+        return L"Invalid Param";
+    case 1:
+        return L"Operation Failed";
+    // a lot more other cases
+    default:
+        return L"N/A";
+    }
+}
+
+int main()
+{
+    auto modifyResult = DoModify();
+    CResult result(GetErrorStr(modifyResult));
+}
